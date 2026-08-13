14a116bf8a33d69cc210054dfa8947bf5d5b7bc0
Asechan Chib <127785782+asechan@users.noreply.github.com>
2026-08-13 21:31:21 +0530

    fixes +2

--- src/components/NoiseOverlay.tsx
+++ src/components/NoiseOverlay.tsx
@@ -16,7 +16,7 @@ export default function NoiseOverlay() {
 
   return (
     <>
-      <div className="fixed inset-0 z-[-1] opacity-70 scale-[1.1]">
+      <div className="fixed top-0 left-0 w-screen h-[100lvh] z-[-1] opacity-70 scale-[1.1]">
         <GradientWaves
           horizonColor={isDark ? "#111112" : "#f7f7f5"}
           waveColor={isDark ? "#1e2621" : "#8ea395"}
